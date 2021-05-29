52d34afc28293f6cf30f2e3d8474e33118a052a7
rajat <rajatjn10@gmail.com>
2021-05-30 01:05:19 +0530

    2nd commit

---
+++ demo.java
@@ -0,0 +1,12 @@
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        
+        System.out.println("You entered: " + number);
+        
+        System.out.println("the git hub");
+    }
+}
