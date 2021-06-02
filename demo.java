845865ad85b5bc988bf47f0d9365d3ebb8dfabd3
rajat <rajatjn10@gmail.com>
2021-06-02 19:36:31 +0530

    3 Commit

--- demo.java
+++ demo.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         
-        System.out.println("You entered: " + number);
+        System.out.println("You entered: ");
         
         System.out.println("the git hub");
     }
